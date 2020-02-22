e7abe5f1bb4bc3fd6ce408f5b4c3c7af8e6b7a1f
Gladibeth <gladibethdelosa@gmail.com>
2020-02-22 11:40:17 -0400

    Add backend

---
+++ wp-content/themes/Retiro-w/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Retiro</title>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/font-awesome.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/slick.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/slick-theme.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+</head>
+<body>
---
+++ wp-content/themes/Retiro-w/index.php
@@ -0,0 +1,176 @@
+<?php get_header();?>
+<section class="main-banner">
+    <div class="main-banner__content">
+      <div class="main-banner__item ">
+        <h1 class="main-banner__watermask"></h1>
+        <div class="main-banner__slider">
+          <div class="main-banner__slideritem">
+            <div class="main-banner__slidercontent">
+              <div class="main-banner__slidermask">
+                <div class="main-banner__sliderbox">
+                  <div class="main-banner__img d-lg-none d-md-none d-sm-none">
+                    <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="">
+                  </div>
+                  <div class="main-banner__slidertext">
+                    
+                      <h1>próximo lanzamiento</h1>
+                      <p>Lote desde <br>
+                        <span>500 Millones de pesos <br></span>
+                      </p>
+                    </div>
+                    <div class="main-banner__slidertextbottom">
+                      <div class="main-banner__sliderubication">
+                        <p>
+                            Rodéate del verde de la naturaleza, respira aire puro en un espacio exclusivo con todas las ventajas de un club privado para el deporte y la diversión,  <strong>zona comercial y la privacidad de nuestros lotes y apartamentos.</strong>
+                        </p>
+                      </div>
+                      
+                      <div class="main-btn__mobile d-lg-none d-md-none d-sm none d-block">
+                        <a class="btn_custom btn--medium btn--filled" href="#contact">
+                          Contáctanos
+                        </a>
+                      </div>
+                    </div>
+                </div>
+              </div>
+
+              <div class="main-banner__sliderimg">
+                <img src="<?php echo get_template_directory_uri();?>/assets/img/banner1.jpeg" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="main-banner__slideritem">
+            <div class="main-banner__slidercontent">
+              <div class="main-banner__slidermask">
+                <div class="main-banner__sliderbox">
+                  <div class="main-banner__img d-lg-none d-md-none d-sm-none">
+                    <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="">
+                  </div>
+                  <div class="main-banner__slidertext">
+                      <h1>próximo lanzamiento</h1>
+                      <p>Lote desde <br>
+                        <span>500 Millones de pesos <br></span>
+                      </p>
+                    </div>
+                    <div class="main-banner__slidertextbottom">
+                      <div class="main-banner__sliderubication">
+                        <p>
+                            Rodéate del verde de la naturaleza, respira aire puro en un espacio exclusivo con todas las ventajas de un club privado para el deporte y la diversión,  <strong>zona comercial y la privacidad de nuestros lotes y apartamentos.</strong>
+                        </p>
+                      </div>
+                      
+                    
+                      <div class="main-btn__mobile d-lg-none d-md-none d-sm none d-block">
+                        <a class="btn_custom btn--medium btn--filled" href="#contact" >
+                          Contáctanos
+                        </a>
+                      </div>
+                    </div>
+                </div>
+              </div>
+
+              <div class="main-banner__sliderimg">
+                <img src="<?php echo get_template_directory_uri();?>/assets/img/banner2.jpeg" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="main-banner__slideritem">
+            <div class="main-banner__slidercontent">
+              <div class="main-banner__slidermask">
+                <div class="main-banner__sliderbox">
+                  <div class="main-banner__img d-lg-none d-md-none d-sm-none">
+                    <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="">
+                  </div>
+                  <div class="main-banner__slidertext">
+                      <h1>próximo lanzamiento</h1>
+                      <p>Lote desde <br>
+                        <span>500 Millones de pesos <br></span>
+                      </p>
+                    </div>
+                    <div class="main-banner__slidertextbottom">
+                      <div class="main-banner__sliderubication">
+                        <p>
+                            Rodéate del verde de la naturaleza, respira aire puro en un espacio exclusivo con todas las ventajas de un club privado para el deporte y la diversión,  <strong>zona comercial y la privacidad de nuestros lotes y apartamentos.</strong>
+                        </p>
+                      </div>
+                      
+                    
+                      <div class="main-btn__mobile d-lg-none d-md-none d-sm none d-block">
+                        <a class="btn_custom btn--medium btn--filled" href="#contact" >
+                          Contáctanos
+                        </a>
+                      </div>
+                    </div>
+                </div>
+              </div>
+
+              <div class="main-banner__sliderimg">
+                <img src="<?php echo get_template_directory_uri();?>/assets/img/banner3.jpeg" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main-banner__item" id="contact">
+        <div class="main-banner__box">
+          <div class="main-banner__img d-none d-sm-block">
+            <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="">
+          </div>
+          <div class="main-banner__form">
+              <div class="main-banner__title">
+                <p>Formulario de Contáctanos</p>
+              </div>
+              <!-- <form action="">
+                <div class="form-group">
+                    <input class="form-control" id="formGroupExampleInput" placeholder="Nombre *" type="text">
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" id="formGroupExampleInput2" placeholder="Correo *" type="text">
+                  </div>
+                  <div class="main-grid">
+                    <div class="form-group">
+                      <input class="form-control" id="formGroupExampleInput2" placeholder="Celular *" type="tel">
+                    </div>
+                    <div class="form-group">
+                      <input class="form-control" id="formGroupExampleInput2" placeholder="Ciudad" type="text">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <textarea placeholder="Mensaje" rows="8"></textarea>
+                  </div>
+                  <div class="custom-control custom-checkbox my-1 mr-sm-2">
+                    <input class="custom-control-input" id="customControlInline" type="checkbox">
+                    <label class="custom-control-label" for="customControlInline">
+                        Acepto
+                      </label>
+                    <a class="text-link" target="_blank" href="<?php echo get_template_directory_uri();?>/assets/img/MANUAL_DE_POLITICAS_PROMOTORA.pdf">
+                        <strong>
+                          política de privacidad
+                        </strong>
+                      </a>
+                  </div>
+                  <div class="float-left btn-box">
+                    <button class="btn_custom btn--medium btn--filled">
+                        Enviar
+                      </button>
+                  </div>
+              </form> -->
+              <!-- <div class="_form_1"></div><script src="https://nivel.activehosted.com/f/embed.php?id=1" 
+                type="text/javascript" charset="utf-8"></script> -->
+                <div class="main-form__active">
+                  <?php echo do_shortcode('[activecampaign form=1]'); ?>
+                  <a class="text-link" target="_blank" href="<?php echo get_template_directory_uri();?>/assets/img/MANUAL_DE_POLITICAS_PROMOTORA.pdf">
+                    <strong>
+                      política de privacidad
+                    </strong>
+                  </a>
+                </div>
+               
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+<?php get_footer();?>
